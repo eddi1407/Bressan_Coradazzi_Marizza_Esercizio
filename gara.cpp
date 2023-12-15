0a27e7cf1e7ffe9f7718cb35dc0601aa37046e32
Eddi Bressan <eddi.bressan@isitgo.it>
2023-12-15 11:48:29 +0100

    fatto

---
+++ gara.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    struct gara{
+        int data;
+        int oraInizio;
+        int pisteDisponibili;
+        int atleti;
+    }
+
+}
